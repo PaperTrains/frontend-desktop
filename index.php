146c231348fb58818a5a40d139896ac71328da99
TimRomeijn <tim28@live.nl>
2017-06-28 00:56:11 +0200

    First version desktop
    First version desktop

--- index.php
+++ index.php
@@ -8,8 +8,24 @@
 </head>
 <body>
     <div class="main-container">
-    
-           
+
+        <div class="sidebar">
+            <img src="./app/images/H1.png" class="sidebar-header">
+            <div class="recently-posted">
+              <h3>Net geplaatst</h3>
+            </div>
+            <div class="recently-liked">
+               <h3>Meest leuk gevonden</h3>
+            </div>
+            <div class="tilted-container">
+                <span class="t-container-text">Doe mee! Bezoek</span><br>
+                <span class="t-container-text">www.papertrains.nl</span>
+            </div>
+        </div>
+
+        <div class="provinces">
+        
+        </div>           
         
     </div>
   <script src="./app/js/scripts.js"></script>
